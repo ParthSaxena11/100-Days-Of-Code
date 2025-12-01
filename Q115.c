8abc417ea9876df66b7186aed430fe451ff8e2b8
ParthSaxena11 <parth.27439@stu.upes.ac.in>
2025-12-01 22:09:07 +0530

    Create Q115.c

---
+++ Q115.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000], t[1000];
+    printf("Enter first string: ");
+    scanf("%s", s);
+    printf("Enter second string: ");
+    scanf("%s", t);
+
+    // If lengths differ, they cannot be anagrams
+    if (strlen(s) != strlen(t)) {
+        printf("Not Anagram\n");
+        return 0;
+    }
+
+    int freq[26] = {0};
+
+    // Count frequency for s
+    for (int i = 0; s[i] != '\0'; i++) {
+        freq[s[i] - 'a']++;
+    }
+
+    // Subtract frequency for t
+    for (int i = 0; t[i] != '\0'; i++) {
+        freq[t[i] - 'a']--;
+    }
+
+    // Check if all counts are zero
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] != 0) {
+            printf("Not Anagram\n");
+            return 0;
+        }
+    }
+
+    printf("Anagram\n");
+    return 0;
+}
