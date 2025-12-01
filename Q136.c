74d597640a78490021a4b2b8385c7b53e63fd312
ParthSaxena11 <parth.27439@stu.upes.ac.in>
2025-12-01 22:16:16 +0530

    Create Q136.c

---
+++ Q136.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+// Define enum for menu choices
+enum Menu { ADD = 1, SUBTRACT, MULTIPLY };
+
+int main() {
+    int choice;
+    int a, b, result;
+
+    // Ask user for two numbers
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    // Show menu
+    printf("\nMenu:\n");
+    printf("1. Add\n");
+    printf("2. Subtract\n");
+    printf("3. Multiply\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    // Perform operation based on choice
+    switch (choice) {
+        case ADD:
+            result = a + b;
+            printf("Result = %d\n", result);
+            break;
+        case SUBTRACT:
+            result = a - b;
+            printf("Result = %d\n", result);
+            break;
+        case MULTIPLY:
+            result = a * b;
+            printf("Result = %d\n", result);
+            break;
+        default:
+            printf("Invalid choice!\n");
+    }
+
+    return 0;
+}
