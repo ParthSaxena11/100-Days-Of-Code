36bde00ecb3129c790d179af8469c3f24385b9b4
ParthSaxena11 <parth.27439@stu.upes.ac.in>
2025-12-01 22:13:13 +0530

    Create Q127.c

---
+++ Q127.c
@@ -0,0 +1,36 @@
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *inFile, *outFile;
+    char ch;
+
+    // Open input file in read mode
+    inFile = fopen("input.txt", "r");
+    if (inFile == NULL) {
+        printf("Error: Cannot open input.txt\n");
+        return 1;
+    }
+
+    // Open output file in write mode
+    outFile = fopen("output.txt", "w");
+    if (outFile == NULL) {
+        printf("Error: Cannot open output.txt\n");
+        fclose(inFile);
+        return 1;
+    }
+
+    // Read each character, convert to uppercase, and write to output
+    while ((ch = fgetc(inFile)) != EOF) {
+        fputc(toupper((unsigned char)ch), outFile);
+    }
+
+    // Close files
+    fclose(inFile);
+    fclose(outFile);
+
+    printf("Conversion complete. Output written to output.txt\n");
+
+    return 0;
+}
